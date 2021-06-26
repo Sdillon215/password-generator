7fdd480078d56a15d19d12b6e5c3a9d0cde973fd
Sean <sdillon215@gmail.com>
2021-06-26 12:50:49 -0700

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function generatePassword() {
         var upperCase = confirm("Would you like to include uppercase letters?");
         var specCharacters = confirm("Would you like to include special characters?");
         var numbers = confirm("Would you like to include numbers?");
-        
+
         // on confirm concat to passwordArr
         if (lowerCase) {
             passwordArr = passwordArr.concat(lowArr);
@@ -32,8 +32,7 @@ function generatePassword() {
         if (numbers) {
             passwordArr = passwordArr.concat(numArr);
         }
-        console.log(length);
-    } 
+    }
     // if incorrect length or NaN is entered alert user and call generatePassword again
     else {
         alert("Please enter a number between 8 and 128.");
@@ -57,9 +56,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-
     passwordText.value = password;
-
 }
 
 // Add event listener to generate button - on click calls writePassword function
